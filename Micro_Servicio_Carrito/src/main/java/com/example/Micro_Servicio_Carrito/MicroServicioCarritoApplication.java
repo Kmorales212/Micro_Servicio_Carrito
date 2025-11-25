11d36e30b7791cbf9a699fbea85247e795ca28d8
Kmorales212 <ke.moralese@duocuc.cl>
2025-11-25 02:14:02 -0300

    conexion con eureka server

--- Micro_Servicio_Carrito/src/main/java/com/example/Micro_Servicio_Carrito/MicroServicioCarritoApplication.java
+++ Micro_Servicio_Carrito/src/main/java/com/example/Micro_Servicio_Carrito/MicroServicioCarritoApplication.java
@@ -1,10 +1,12 @@
 package com.example.Micro_Servicio_Carrito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.example.carrito")
 @EnableJpaRepositories(basePackages = "com.example.carrito.repository")
 public class MicroServicioCarritoApplication { 
